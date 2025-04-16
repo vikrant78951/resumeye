daabc2122649bdf92dfcf4fe9230d68320d079cf
vikrant78951 <vikrant78951@gmail.com>
2025-04-16 19:15:43 +0530

    added playwrite configs

--- src/root/tests/home.test.tsx
+++ src/root/tests/home.test.tsx
@@ -1,10 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import { describe, it, expect } from 'vitest';
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
 
-describe('False Test Case', () => {
-    it('renders a welcome message', () => {
-        render(<h1>Welcome to Resumeye</h1>);
-        const message = screen.getByText(/Welcome to Resumeye/i);
-        expect(message).toBeDefined();
-    });
+describe("False Test Case", () => {
+  it("renders a welcome message", () => {
+    render(<h1>Welcome to Resumeye</h1>);
+    const message = screen.getByText(/Welcome to Resumeye/i);
+    expect(message).toBeDefined();
+  });
 });
--- vitest.config.mts
+++ vitest.config.mts
@@ -6,5 +6,6 @@ export default defineConfig({
   plugins: [tsconfigPaths(), react()],
   test: {
     environment: 'jsdom',
+    exclude: ['__playwright_test__','node_modules'],
   },
 })
